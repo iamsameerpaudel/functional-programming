e98c045a5e3d628ae7c02d89f6c84e22a8c41ad5
Sameer Paudel <samir.poudel@truenary.com>
2025-06-04 11:13:47 +0545

    feat: add description document for arity and demonstrate it

---
+++ arity/arity.js
@@ -0,0 +1,4 @@
+function arity(a, b, c) { }
+console.log(arity.length);
+function arityLength5(a, b, c, d, e) { }
+console.log(arityLength5.length);
---
+++ arity/arity.ts
@@ -0,0 +1,6 @@
+function arity(a,b,c){}
+console.log(arity.length)
+
+
+function arityLength5(a,b,c,d,e){}
+console.log(arityLength5.length)
